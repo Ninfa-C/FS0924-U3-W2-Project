e5b61010dc167c00b19203c0b4fd915a96c51535
Ninfa-C <ninfa_95@hotmail.it>
2025-01-18 17:57:07 +0100

    welcome page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,39 @@
 import { Container } from "react-bootstrap";
 
 const HomePage = () => {
-
   return (
-    <Container fluid className='d-flex flex-column'>
+    <Container fluid className="d-flex flex-column">
+      <h1>Benvenut* nella mia prima pagina meteo</h1>
+      <h2 className=" fst-italic opacity-50 fs-3">Ninfa Lissette Carreno Jacho</h2>
+      <hr />
+      <p className="mb-3">
+        Cerca la città che preferisci nella barra di ricerca e selziona, tra i
+        risultati ottenuti, la città che desideri. Otterrai infromazioni su:
+      </p>
+      <ul>
+        <li className="mb-3"> Meteo e temperatura corrente </li>
+        <li className="mb-3"> Meteo e temperatura dei prossimi 6 giorni</li>
+        <li className="mb-3">Meteo e temperatura ogni 3 ore per i prissimi 4 giorni</li>
+        <li className="mb-2">
+          Informazioni varie:
+          <ul >
+            <li className="my-2">Real Feel</li>
+            <li className="mb-2">Vento</li>
+            <li className="mb-2">Probabilità di pioggia</li>
+            <li className="mb-2">Livello di umidità</li>
+            <li className="mb-2">Visibilità</li>
+            <li className="mb-2">Pressione</li>
+            <li className="mb-2"> Alba</li>
+            <li className="">Tramonto</li>
+          </ul>
+        </li>
+      </ul>
+
+      <p className="mb-3">
+        Ho impostato di default una ricerca in <strong>Cities: Londra</strong> e
+        in <strong>Weather: Torino</strong>
+      </p>
+      <p>Buon proseguimento!</p>
     </Container>
   );
 };
